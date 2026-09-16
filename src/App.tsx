0ae4da2e88f63ce4c0cbbab10b2d2a6ba8f635fa
Demetrius Harvey <78282284+demetriusvharvey@users.noreply.github.com>
2026-09-16 13:53:50 -0400

    feat: add Shopify product and cart routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
+import Sneaker from "./pages/Sneaker";
+import Cart from "./pages/Cart";
 import SellTrade from "./pages/SellTrade";
 import Contact from "./pages/Contact";
-
-// Placeholder pages (will be built out next)
-function NewArrivals() {
-  return <Shop />;
-}
-function Brands() {
-  return <Shop />;
-}
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/shop" element={<Shop />} />
-      <Route path="/new-arrivals" element={<NewArrivals />} />
-      <Route path="/brands" element={<Brands />} />
+      <Route path="/new-arrivals" element={<Shop />} />
+      <Route path="/brands" element={<Shop />} />
+      <Route path="/products/:handle" element={<Sneaker />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/checkout" element={<Navigate to="/cart" replace />} />
       <Route path="/sell-trade" element={<SellTrade />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
